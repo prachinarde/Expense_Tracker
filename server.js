ec7fab5e866a2c8480f1ccaf2b7821c95e356e2b
prachinarde <prachinarde79@gmail.com>
2024-08-15 00:47:47 +0530

    7

--- server.js
+++ server.js
@@ -12,17 +12,15 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(cors());
 //route for user
-
+app.use(express.static(path.join(__dirname, './client/build')));
 app.use("/api/v1/users", require('./routes/userRoute'));
 
 
-
-//rout for transaction
-app.use("/api/v1/transactions", require("./routes/transactionRoutes"));
 app.get("*", function(req, res){
     res.sendFile(path.join(__dirname, './client/build/index.html'));
 })
-app.use(express.static(path.join(__dirname, './client/build')));
+//rout for transaction
+app.use("/api/v1/transactions", require("./routes/transactionRoutes"));
 const PORT = 5000 || process.env.PORT;
 
 app.listen(PORT, () => {
